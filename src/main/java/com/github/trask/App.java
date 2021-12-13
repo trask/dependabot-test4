070a815d0afc9dc78a9a9d510c7beb5e7569ff7e
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-13 13:03:20 -0800

    Initial commit

---
+++ src/main/java/com/github/trask/App.java
@@ -0,0 +1,13 @@
+package com.github.trask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
